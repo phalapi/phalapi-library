102ba723ccacf73a540d366ff4f274c0639deaed
dogstar <chanzonghuang@gmail.com>
2015-05-19 23:18:36 +0800

    more task

--- Task/Tests/test_env.php
+++ Task/Tests/test_env.php
@@ -4,6 +4,7 @@ $_GET['debug'] = $_GET['__sql__'] = 1;
 //require_once dirname(__FILE__) . '/../../../Public/init.php';
 require_once '/home/dogstar/projects/library.phalapi.net/Public/init.php';
 
+DI()->loader->addDirs('Demo');
 DI()->loader->addDirs('Library');
 DI()->loader->addDirs('./Library/Task/Task');
 
