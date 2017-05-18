ba2a4638b4d0025da09c218050d03b36c64ecf73
dogstar <chanzonghuang@gmail.com>
2017-05-18 19:47:13 +0800

    config file

---
+++ Qiniu/Config/app.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+
+    /**
+     * 七牛相关配置
+     */
+    'Qiniu' =>  array(
+        //统一的key
+        'accessKey' => '*****',
+        'secretKey' => '****',
+        //自定义配置的空间
+        'space_bucket' => '自定义配置的空间',
+        'space_host' => 'http://XXXXX.qiniudn.com',
+    ),
+);
